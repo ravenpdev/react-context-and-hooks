1283971d20e4e1408305b333627112b1c6cf1804
ravenpdev <ravenp.dev@gmail.com>
2020-08-10 22:59:52 +0800

    convert ThemeToggle.js to use useContext

--- src/components/ThemeToogle.js
+++ src/components/ThemeToogle.js
@@ -1,21 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 const ThemeToggle = () => {
+  const { toggleTheme } = useContext(ThemeContext);
+
   return (
-    <ThemeContext.Consumer>
-      {(context) => {
-        const { toggleTheme } = context;
-        return (
-          <button
-            className="p-2 text-sm text-white bg-blue-700 rounded"
-            onClick={toggleTheme}
-          >
-            Toggle theme
-          </button>
-        );
-      }}
-    </ThemeContext.Consumer>
+    <button
+      className="p-2 text-sm text-white bg-blue-700 rounded"
+      onClick={toggleTheme}
+    >
+      Toggle theme
+    </button>
   );
 };
 
